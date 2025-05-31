2f7f34801435b5b2a9de295f5e86578a8fbfe4a6
Rheveni <125421564+Rheveni@users.noreply.github.com>
2025-05-31 20:35:51 -0300

    Adicionado comentários para melhor entendimento do código

--- cypress/e2e/product.spec.cy.js
+++ cypress/e2e/product.spec.cy.js
@@ -2,7 +2,7 @@ import LoginPage from "../pages/loginPage"
 import userData from "../fixtures/userData.json"
 import ProductPage from "../pages/productPage"
 
-
+// Instancia os objetos das páginas para reutilização
 const loginPage = new LoginPage()
 const productPage = new ProductPage()
 
@@ -11,11 +11,13 @@ const productPage = new ProductPage()
   describe('Product Test', () => {
     it('Select Product - Sucess', () => {
       cy.visit('https://www.saucedemo.com/')
-  
+
+      // Realiza o login com dados válidos
         loginPage.loginWithUser(userData.userSuccess.userName, userData.userSuccess.password)
         loginPage.loginButton()
         loginPage.homeAlert()
 
+      // Seleciona o produto desejado, valida informações e adiciona produto no carrinho
         productPage.selectProduct(userData.descriptionProduct.itemName, userData.descriptionProduct.itemPrice, userData.descriptionProduct.itemDescription)
         productPage.clickAddToCartButton()
    
