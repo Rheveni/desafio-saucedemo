4c894fadbeed2748d5b23ac640fcf7adc63fdc9a
Rheveni <125421564+Rheveni@users.noreply.github.com>
2025-05-31 20:59:29 -0300

    Adicionado comentários para melhor entendimento do código

--- cypress/pages/checkoutPage.js
+++ cypress/pages/checkoutPage.js
@@ -1,3 +1,5 @@
+// Page Object para interações no processo de checkout
+
 class CheckoutPage{
 
     selectorsList(){
@@ -17,7 +19,7 @@ class CheckoutPage{
        // cy.get('body').should('contain', 'Checkout: Overview')
     }
 
-
+// Preenche os dados do formulário de checkout
     fillCheckoutForm(firstName, lastName, postalCode) {
         
         cy.get(this.selectorsList().firstNameInput).type(firstName)
@@ -25,18 +27,24 @@ class CheckoutPage{
         cy.get(this.selectorsList().postalCodeInput).type(postalCode)
     }
 
+
+
+  // Valida se os dados inseridos estão corretos
     verifyCheckoutForm(firstName, lastName, postalCode) {
         cy.get(this.selectorsList().firstNameInput).should('have.value', firstName)
         cy.get(this.selectorsList().lastNameInput).should('have.value', lastName)
         cy.get(this.selectorsList().postalCodeInput).should('have.value', postalCode)
     }
 
+
+// Clica no botão Continue para prosseguir com o pedido
     clickContinueButton(){
         cy.get(this.selectorsList().continueButton).click()
         cy.get('body').should('contain', 'Checkout: Overview')
     }
 
 
+// Clica no botão Finish para concluir o pedido
     clickFinishButton() {
         cy.get(this.selectorsList().finishButton).click()
         cy.get('body').should('contain', 'Checkout: Complete!')
