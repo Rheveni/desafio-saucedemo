e4cf4fd8eb6a64f1ef3a6ad1674d3408ea54b124
Rheveni <125421564+Rheveni@users.noreply.github.com>
2025-05-31 20:34:45 -0300

    Adicionado comentários para melhor entendimento do código

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,7 +1,7 @@
 import LoginPage from "../pages/loginPage"
 import userData from "../fixtures/userData.json"
 
-
+// Instancia os objetos das páginas para reutilização
 const loginPage = new LoginPage()
 
 
@@ -9,6 +9,7 @@ describe('Login Test', () => {
   it('Login - Sucess', () => {
     cy.visit('https://www.saucedemo.com/')
 
+    // Realiza o login com dados válidos
     loginPage.loginWithUser(userData.userSuccess.userName, userData.userSuccess.password)
     loginPage.loginButton()
     loginPage.homeAlert()
