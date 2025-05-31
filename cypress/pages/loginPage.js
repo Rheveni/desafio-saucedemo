304de9952ab553e246d05ca88b38e14a98ee6650
Rheveni <125421564+Rheveni@users.noreply.github.com>
2025-05-31 20:45:39 -0300

    Adicionando comentários para melhor entendimento do código

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -10,15 +10,21 @@ class LoginPage{
 }
 return selectors
     }
+
+// Preenche os campos de login com os dados fornecidos
     loginWithUser(username, password) {
         cy.get(this.selectorsList().usernameField).type(username);
         cy.get(this.selectorsList().passwordField).type(password);
     }
 
+
+ // Clica no botão de login
     loginButton(){
         cy.get(this.selectorsList().loginButton).click();
     }
 
+
+// Valida se o login foi bem-sucedido
     homeAlert() {
         cy.get('body').should('contain', 'Products');
     }
